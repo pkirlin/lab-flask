e51901c20ee08c74ca5cad88f08c95676e6c6fb5
pkirlin <pkirlin@users.noreply.github.com>
2016-11-16 11:07:38 -0600

    Update populate.sql

--- blog-browse-write-edit-delete/populate.sql
+++ blog-browse-write-edit-delete/populate.sql
@@ -1,3 +1,5 @@
+delete from entries;
+
 insert into entries(date, title, content) values (datetime('now', '-10 days', 'localtime'), 'First post!', 'This is my first post.  It is exciting!');
 insert into entries(date, title, content) values (datetime('now', '-1 days', 'localtime'), 'I love Flask', 'I am finding Flask incredibly fun.');
 insert into entries(date, title, content) values (datetime('now', '-1 minute', 'localtime'), 'Databases', 'My databases class is a lot of work, but I am enjoying it.');
