3e9b5c58fe205422511d1464173a9e1b999defd8
Phil Kirlin <pkirlin@users.noreply.github.com>
2019-10-09 16:32:40 -0500

    Update blog.py

--- blog-browse-write-edit-delete/blog.py
+++ blog-browse-write-edit-delete/blog.py
@@ -1,8 +1,12 @@
 # Blog Flask application
 
 import os
-import sqlite3
+import psycopg2
+import psycopg2.extras
 from flask import Flask, request, render_template, g
+
+# PostgreSQL IP address
+IP_ADDR = "YOUR IP ADDRESS"
 
 # Create the application
 app = Flask(__name__)
@@ -17,25 +21,25 @@ def homepage():
 @app.route("/dump")
 def dump_entries():
     db = get_db()
-    rows = db.execute('select id, date, title, content from entries order by date')
+    cursor = db.cursor()
+    cursor.execute('select id, date, title, content from entries order by date')
+    rows = cursor.fetchall()
     output = ""
     for r in rows:
         debug(str(dict(r)))
         output += str(dict(r))
         output += "\n"
-    return "<pre>" + output + "</pre>"
-    
+
 @app.route("/browse")
 def browse():
     db = get_db()
-    rows = db.execute('select id, date, title, content from entries order by date')
-    rowlist = rows.fetchall()
+    cursor = db.cursor()
+    cursor.execute('select id, date, title, content from entries order by date')
+    rowlist = cursor.fetchall()
     return render_template('browse.html', entries=rowlist)
-    
+
 @app.route("/write", methods=['get', 'post'])
 def write():
-    debug("form data=" + str(request.form))
-
     # Step 1, display form
     if "step" not in request.form:     
         return render_template('write.html', step="compose_entry")
@@ -43,41 +47,44 @@ def write():
     # Step 2, add blog post to database.
     elif request.form["step"] == "add_entry":
         db = get_db()
-        db.execute("insert into entries (date, title, content) values (datetime('now', 'localtime'), ?, ?)",
+        cursor = db.cursor()
+        cursor.execute("insert into entries (title, content) values (%s, %s)",
                    [request.form['title'], request.form['content']])
         db.commit()
         return render_template("write.html", step="add_entry")
-        
+
 @app.route("/edit", methods=['get', 'post'])
 def edit():
     debug("form data=" + str(request.form))
-
     # Step 1, display form to select which entry to edit
     if "step" not in request.form:
         db = get_db()
-        rows = db.execute('select id, date, title, content from entries order by date')
-        rowlist = rows.fetchall()
+        cursor = db.cursor()
+        cursor.execute('select id, date, title, content from entries order by date')
+        rowlist = cursor.fetchall()
         return render_template('edit.html', step="display_entries", entries=rowlist)
     
     # Step 2, get postID from form, SELECT this post from the db, and display
-    # a form to edit that post.
+    # a form to edit that post.    
     elif request.form["step"] == "make_edits":
         db = get_db()
+        cursor = db.cursor()
         # get the postID from the form
         postid = int(request.form["postid"])
         debug("Using postid=" + str(postid))
         
         # query the DB to retrieve that post by ID.  We use fetchone()
         # to retrieve the only row (there can be only one!)
-        row = db.execute("select id, date, title, content from entries where id=?", [postid])
-        therow = row.fetchone()
-        debug("db retrieved: " + str(dict(therow)))
+        cursor.execute("select id, date, title, content from entries where id=%s", [postid])
+        row = cursor.fetchone()
+        debug("db retrieved: " + str(dict(row)))
         
-        return render_template("edit.html", step="make_edits", entry=therow)
+        return render_template("edit.html", step="make_edits", entry=row)
         
     # Step 3, user has changed post, now update the DB with changes
     elif request.form["step"] == "update_database":
         db = get_db()
+        cursor = db.cursor()
         
         # get the postID from the form
         postid = int(request.form["postid"])
@@ -90,14 +97,14 @@ def edit():
         
         # run our UPDATE
         if changedate:
-            db.execute("update entries set title=?, content=?, date=datetime('now', 'localtime') where id=?",
+            cursor.execute("update entries set title=%s, content=%s, date=now() where id=%s",
                        [request.form['title'], request.form['content'], postid])
         else:
-            db.execute("update entries set title=?, content=? where id=?", 
+            cursor.execute("update entries set title=%s, content=%s where id=%s", 
                        [request.form['title'], request.form['content'], postid])
         db.commit()
         return render_template("edit.html", step="update_database")
-        
+
 @app.route("/delete", methods=['get', 'post'])
 def delete():
     debug("form data=" + str(request.form))
@@ -105,22 +112,23 @@ def delete():
     # Step 1, display form to select which entry to delete
     if "step" not in request.form:
         db = get_db()
-        rows = db.execute('select id, date, title, content from entries order by date')
-        rowlist = rows.fetchall()
+        cursor = db.cursor()
+        cursor.execute('select id, date, title, content from entries order by date')
+        rowlist = cursor.fetchall()
         return render_template('delete.html', step="display_entries", entries=rowlist)
         
     # Step 2, user has changed post, now update the DB with changes
     elif request.form["step"] == "delete_entry":
         db = get_db()
+        cursor = db.cursor()
         
         # get the postID from the form
         postid = int(request.form["postid"])
         
         # run our DELETE
-        db.execute("delete from entries where id=?", [postid])
+        cursor.execute("delete from entries where id=%s", [postid])
         db.commit()
         return render_template("delete.html", step="delete_entry")
-    
 
 #####################################################
 # Database handling 
@@ -128,25 +136,25 @@ def delete():
 def connect_db():
     """Connects to the database."""
     debug("Connecting to DB.")
-    conn = sqlite3.connect(os.path.join(app.root_path, 'blog.db'))
-    conn.row_factory = sqlite3.Row
+    conn = psycopg2.connect(host=IP_ADDR, user="postgres", password="rhodes", dbname="blogdb", 
+        cursor_factory=psycopg2.extras.DictCursor)
     return conn
     
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
     """
-    if not hasattr(g, 'db_connection'):
-        g.db_connection = connect_db()
-    return g.db_connection
+    if not hasattr(g, 'pg_db'):
+        g.pg_db = connect_db()
+    return g.pg_db
     
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database automatically when the application
     context ends."""
     debug("Disconnecting from DB.")
-    if hasattr(g, 'db_connection'):
-        g.db_connection.close()
+    if hasattr(g, 'pg_db'):
+        g.pg_db.close()
 
 ######################################################
 # Command line utilities 
@@ -154,7 +162,7 @@ def close_db(error):
 def init_db():
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
+        db.cursor().execute(f.read())
     db.commit()
 
 @app.cli.command('initdb')
@@ -162,11 +170,11 @@ def init_db_command():
     """Initializes the database."""
     print("Initializing DB.")
     init_db()
-    
+
 def populate_db():
     db = get_db()
     with app.open_resource('populate.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
+        db.cursor().execute(f.read())
     db.commit()
 
 @app.cli.command('populate')
